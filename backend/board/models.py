5d6345124dfee0777cf2099644b29111ed03ed1f
korea7030 <korea7030@naver.com>
2021-02-15 20:44:57 +0900

    django react board init

---
+++ backend/board/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from accounts.models import CustomUser
+
+
+class Post(models.Model):
+    '''
+    게시글
+    '''
+    title = models.CharField(max_length=200, default='')
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.SET_NULL)
+    hit = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    '''
+    게시글 댓글, 대댓글(use parent)
+    '''
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, related_name='comments', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', related_name='reply', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100, default='')
+    created_at = models.DateTimeField('생성시간', auto_now_add=True)
